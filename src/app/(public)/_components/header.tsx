5114c40b63eddb84bafc3a5cbfb88c7c16643629
fernandohiroshi <contatofernandohiroshitakeda@outlook.com>
2025-06-10 18:25:30 -0300

    feat(header): add NavLinks

--- src/app/(public)/_components/header.tsx
+++ src/app/(public)/_components/header.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Sheet,
@@ -7,10 +9,35 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { Menu } from "lucide-react";
+import { Handshake, Mail, Menu } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
 
 export function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const navItems = [
+    { href: "#profissionais", label: "Profissionais", icon: Handshake },
+    { href: "/contato", label: "Contatos", icon: Mail },
+  ];
+
+  const NavLinks = () => (
+    <>
+      {navItems.map((item) => (
+        <Button
+          onClick={() => setIsOpen(false)}
+          key={item.href}
+          asChild
+          className="bg-transparent hover:bg-transparent text-neutral-900 shadow-none"
+        >
+          <Link href={item.href} className="flex items-center">
+            <item.icon className="text-cyan-500" /> {item.label}
+          </Link>
+        </Button>
+      ))}
+    </>
+  );
+
   return (
     <header className="fixed top-0 right-0 left-0 z-[999] py-4 px-4">
       <div className="container mx-auto flex items-center justify-between">
@@ -18,11 +45,13 @@ export function Header() {
           Dent<span className="text-cyan-500">PRO</span>
         </Link>
 
-        <nav className="hidden md:flex items-center">
-          <a href="#">Profissionais</a>
+        {/* DESKTOP */}
+        <nav className="hidden md:flex items-center space-x-4">
+          <NavLinks />
         </nav>
 
-        <Sheet>
+        {/* MOBILE */}
+        <Sheet open={isOpen} onOpenChange={setIsOpen}>
           <SheetTrigger asChild className="md:hidden">
             <Button
               className="text-neutral-900 hover:bg-transparent"
@@ -39,11 +68,10 @@ export function Header() {
           >
             <SheetHeader>
               <SheetTitle>Menu</SheetTitle>
-              <SheetDescription>Veja nossos links</SheetDescription>
             </SheetHeader>
 
-            <nav className="hidden md:flex items-center">
-              <a href="#">Profissionais</a>
+            <nav className="flex flex-col items-start md:hidden">
+              <NavLinks />
             </nav>
           </SheetContent>
         </Sheet>
