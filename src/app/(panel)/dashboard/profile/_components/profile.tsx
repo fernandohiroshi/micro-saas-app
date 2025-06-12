9a3b9444ec105ec6174dff221d17ea64176421c0
fernandohiroshi <contatofernandohiroshitakeda@outlook.com>
2025-06-12 12:10:11 -0300

    feat(profile): create form

--- src/app/(panel)/dashboard/profile/_components/profile.tsx
+++ src/app/(panel)/dashboard/profile/_components/profile.tsx
@@ -3,6 +3,7 @@
 import { useProfileForm } from "./profile-form";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
+import img from "../../../../../../public/home-grid/02.jpg";
 
 import {
   Form,
@@ -21,8 +22,114 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Image from "next/image";
 
 export function ProfileContent() {
   const form = useProfileForm();
-  return <div>profile content</div>;
+
+  return (
+    <section className="mx-auto">
+      <Form {...form}>
+        <form>
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-xl uppercase">Meus dados</CardTitle>
+            </CardHeader>
+            <CardContent className="space-y-6">
+              <div className="flex justify-center">
+                <div className="relative h-40 w-40 rounded-full overflow-hidden shadow">
+                  <Image
+                    src={img}
+                    alt="Profile Image"
+                    fill
+                    className="object-cover"
+                  />
+                </div>
+              </div>
+
+              <div className="space-y-4">
+                {/* NAME */}
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="font-semibold">Nome</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder="Digite o nome da clínica ..."
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+
+                {/* ADDRESS */}
+                <FormField
+                  control={form.control}
+                  name="address"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="font-semibold">Endereço</FormLabel>
+                      <FormControl>
+                        <Input {...field} placeholder="Digite o endereço ..." />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+
+                {/* PHONE */}
+                <FormField
+                  control={form.control}
+                  name="phone"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="font-semibold">Telefone</FormLabel>
+                      <FormControl>
+                        <Input {...field} placeholder="Digite o telefone ..." />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+
+                {/* STATUS */}
+                <FormField
+                  control={form.control}
+                  name="status"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="font-semibold">
+                        Status da clínica
+                      </FormLabel>
+                      <FormControl>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value ? "active" : "inactive"}
+                        >
+                          <SelectTrigger>
+                            <SelectValue placeholder="Selecione o status da clínica" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            <SelectItem value="active">
+                              Clínica aberta (Ativo)
+                            </SelectItem>
+
+                            <SelectItem value="inactive">
+                              Clínica fechada (Inativo)
+                            </SelectItem>
+                          </SelectContent>
+                        </Select>
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </CardContent>
+          </Card>
+        </form>
+      </Form>
+    </section>
+  );
 }
