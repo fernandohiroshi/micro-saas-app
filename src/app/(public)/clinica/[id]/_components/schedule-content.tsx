230525362f2b293211529e8d0aa8d6433b2a9bf2
fernandohiroshi <contatofernandohiroshitakeda@outlook.com>
2025-06-15 18:39:41 -0300

    feat(schedule): add DateTimePicker

--- src/app/(public)/clinica/[id]/_components/schedule-content.tsx
+++ src/app/(public)/clinica/[id]/_components/schedule-content.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import imageTest from "../../../../../../public/user.png";
-import { Divide, MapPin } from "lucide-react";
+import { MapPin } from "lucide-react";
 import { Prisma } from "@/generated/prisma";
 import { useAppointmentForm, AppointmentFormDate } from "./schedule-form";
 import {
@@ -13,8 +13,11 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Label } from "@/components/ui/label";
+
 import { Input } from "@/components/ui/input";
+import { formatPhone } from "@/utils/formatPhone";
+import DateTimePicker from "./date-picker";
+import "react-datepicker/dist/react-datepicker.css";
 
 type UserWithServiceAndSubscription = Prisma.UserGetPayload<{
   include: {
@@ -31,13 +34,13 @@ export default function ScheduleContent({ clinic }: ScheduleContentProps) {
   const form = useAppointmentForm();
 
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-neutral-100">
       <div className="h-32 bg-cyan-600" />
 
       <section className="container mx-auto px-4 -mt-16">
         <div className="max-w-2xl mx-auto">
           <article className="flex flex-col items-center">
-            <div className="relative h-48 w-48 rounded-full overflow-hidden shadow bg-white mb-8 border-2">
+            <div className="relative h-48 w-48 rounded-full overflow-hidden shadow bg-white mb-8 border-3">
               <Image
                 src={clinic.image ? clinic.image : imageTest}
                 alt="Foto da clínica"
@@ -58,31 +61,103 @@ export default function ScheduleContent({ clinic }: ScheduleContentProps) {
       </section>
 
       {/* Form Schedule */}
-      <Form {...form}>
-        <form className="space-y-6 bg-white p-6 shadow border rounded-md mx-2">
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <div>
-                <FormItem className="my-2">
-                  <FormLabel className="font-semibold">
-                    Nome completo:
-                  </FormLabel>
-                  <FormControl>
-                    <Input
-                      id="name"
-                      placeholder="Digite seu nome completo"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              </div>
-            )}
-          />
-        </form>
-      </Form>
+      <section className="max-w-2xl mx-auto mt-6 w-full">
+        <Form {...form}>
+          <form className="space-y-6 p-6 shadow-xl border rounded-md mx-2">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <div>
+                  <FormItem className="my-2">
+                    <FormLabel className="font-semibold">
+                      Nome completo:
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        id="name"
+                        placeholder="Digite seu nome completo..."
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                </div>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <div>
+                  <FormItem className="my-2">
+                    <FormLabel className="font-semibold">E-mail:</FormLabel>
+                    <FormControl>
+                      <Input
+                        id="email"
+                        placeholder="Digite seu e-mail..."
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                </div>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="phone"
+              render={({ field }) => (
+                <div>
+                  <FormItem className="my-2">
+                    <FormLabel className="font-semibold">Telefone:</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        id="phone"
+                        placeholder="(xx) xxxxx-xxxx"
+                        onChange={(e) => {
+                          const formattedValue = formatPhone(e.target.value);
+                          field.onChange(formattedValue);
+                        }}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                </div>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="date"
+              render={({ field }) => (
+                <div>
+                  <FormItem className="my-2 flex items-center gap-2">
+                    <FormLabel className="font-semibold">
+                      Data do agendamento:
+                    </FormLabel>
+                    <FormControl>
+                      <DateTimePicker
+                        initialDate={new Date()}
+                        className="w-full rounded border p-2"
+                        onChange={(date) => {
+                          if (date) {
+                            field.onChange(date);
+                          }
+                        }}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                </div>
+              )}
+            />
+          </form>
+        </Form>
+      </section>
     </div>
   );
 }
