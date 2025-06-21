a9794155f8c76568d5b1dcaffa97359d8c3e3843
fernandohiroshi <contatofernandohiroshitakeda@outlook.com>
2025-06-20 22:50:24 -0300

    feat(dashboard): create grid in AppointmentsList

--- src/app/(panel)/dashboard/_components/appointments/appointments-list.tsx
+++ src/app/(panel)/dashboard/_components/appointments/appointments-list.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { useSearchParams } from "next/navigation";
 
 interface AppointmestListProps {
@@ -12,5 +14,34 @@ export default function AppointmentsList({ times }: AppointmestListProps) {
 
   console.log(date);
 
-  return <div>lista de horarios</div>;
+  return (
+    <Card>
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="text-xl md:text-2xl font-bold">
+          Agendamentos
+        </CardTitle>
+
+        <button>SELECIONAR DATA</button>
+      </CardHeader>
+
+      <CardContent>
+        <ScrollArea className="h-[calc(100vh-20rem)] lg:h-[calc(100vh-15rem)] pr-4">
+          {times.map((slot) => {
+            return (
+              <div
+                key={slot}
+                className="flex items-center py-2 border-t last:border-b"
+              >
+                <div className="w-16 text-sm font-semibold">{slot}</div>
+
+                <div className="flex-1 text-sm text-neutral-700">
+                  Disponível
+                </div>
+              </div>
+            );
+          })}
+        </ScrollArea>
+      </CardContent>
+    </Card>
+  );
 }
