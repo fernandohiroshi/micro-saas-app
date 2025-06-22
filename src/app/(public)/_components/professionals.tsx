77a489cdfd38ae55cb3e46cd90c611c5dbf3a63a
fernandohiroshi <contatofernandohiroshitakeda@outlook.com>
2025-06-22 01:34:25 -0300

    feat(home): list clinics on Home

--- src/app/(public)/_components/professionals.tsx
+++ src/app/(public)/_components/professionals.tsx
@@ -4,59 +4,14 @@ import Link from "next/link";
 import { ArrowRight } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
-import img01 from "../../../../public/home-grid/01.jpg";
-import img02 from "../../../../public/home-grid/02.jpg";
-import img03 from "../../../../public/home-grid/03.jpg";
-import img04 from "../../../../public/home-grid/04.jpg";
-import img05 from "../../../../public/home-grid/05.jpg";
-import img06 from "../../../../public/home-grid/06.jpg";
-import img07 from "../../../../public/home-grid/07.jpg";
-import img08 from "../../../../public/home-grid/08.jpg";
+import img from "../../../../public/user.png";
+import { User } from "@/generated/prisma";
 
-const clinics = [
-  {
-    image: img01,
-    name: "Clínica Vida Nova",
-    address: "Av. Paulista, São Paulo - SP",
-  },
-  {
-    image: img02,
-    name: "Clínica Horizonte",
-    address: "Centro, Florianópolis - SC",
-  },
-  {
-    image: img03,
-    name: "Clínica Bem Estar",
-    address: "Savassi, Belo Horizonte - MG",
-  },
-  {
-    image: img04,
-    name: "Clínica Ser Saúde",
-    address: "Boa Viagem, Recife - PE",
-  },
-  {
-    image: img05,
-    name: "Clínica Equilíbrio",
-    address: "Jardins, São Paulo - SP",
-  },
-  {
-    image: img06,
-    name: "Clínica Nova Esperança",
-    address: "Água Verde, Curitiba - PR",
-  },
-  {
-    image: img07,
-    name: "Clínica Plenitude",
-    address: "Centro, Porto Alegre - RS",
-  },
-  {
-    image: img08,
-    name: "Clínica Vitalis",
-    address: "Asa Sul, Brasília - DF",
-  },
-];
+interface ProfessionalsProps {
+  professionals: User[];
+}
 
-export default function Professionals() {
+export default function Professionals({ professionals }: ProfessionalsProps) {
   return (
     <section className="py-8 md:py-16">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
@@ -65,19 +20,19 @@ export default function Professionals() {
         </h2>
 
         <section className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {clinics.map((clinic, index) => (
+          {professionals.map((clinic) => (
             <Card
-              className="p-0 hover:bg-cyan-100 ease-in-out duration-500"
-              key={index}
+              className="p-0 md:hover:shadow-xl ease-in-out duration-300"
+              key={clinic.id}
             >
               <CardContent className="p-0">
                 <div>
                   <div className="relative h-56 rounded-t-lg overflow-hidden">
                     <Image
-                      src={clinic.image}
-                      alt={`Foto da ${clinic.name}`}
+                      src={clinic.image ?? img}
+                      alt="Imagem da clínica"
                       fill
-                      className="object-cover hover:scale-110 ease-in-out duration-500"
+                      className="object-cover"
                     />
                   </div>
                 </div>
@@ -87,7 +42,7 @@ export default function Professionals() {
                     <div>
                       <h3 className="font-semibold">{clinic.name}</h3>
                       <p className="text-sm text-neutral-700">
-                        {clinic.address}
+                        {clinic.address ?? "Endereço não informado."}
                       </p>
                     </div>
 
@@ -95,7 +50,7 @@ export default function Professionals() {
                   </div>
 
                   <Button asChild className="flex items-centerw-full" size="sm">
-                    <Link href="#">
+                    <Link href={`/clinica/${clinic.id}`}>
                       Agendar horário
                       <ArrowRight />
                     </Link>
